89b9da29c7ef1cb6d7cd7ad7fcfa8cb769ce2158
AlanSoaresDeOliveira <alan.oliveira19@fatec.sp.gov.br>
2020-05-21 11:49:54 -0300

    Criando teste para Update do Avatar do usuário

--- src/modules/users/services/UpdateUserAvatarService.spec.ts
+++ src/modules/users/services/UpdateUserAvatarService.spec.ts
@@ -19,6 +19,28 @@ describe('UpdateUserAvatar', () => {
       email: 'johndoe@example.com',
       password: '123456',
     });
-    expect(user).toHaveProperty('id');
+
+    await updateUserAvatar.execute({
+      user_id: user.id,
+      avatarFilename: 'avatar.jpg',
+    });
+    expect(user.avatar).toBe('avatar.jpg');
+  });
+
+  it('should be able to update avatar from non existing user', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeStorageProvider = new FakeStorageProvider();
+
+    const updateUserAvatar = new UpdateUserAvatarService(
+      fakeUsersRepository,
+      fakeStorageProvider,
+    );
+
+    expect(
+      updateUserAvatar.execute({
+        user_id: 'non-existing-user',
+        avatarFilename: 'avatar.jpg',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
